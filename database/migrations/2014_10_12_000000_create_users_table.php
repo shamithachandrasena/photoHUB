548e60cc32a2422d200d6d600b93a622b0d28424
ChiranthaJananath <chiranthjtk@gmail.com>
2018-08-28 02:14:11 +0530

    Login with registration, but social login part can't find the error, i think the error cooming from the social media sites for the security resons

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique()->nullable();
             $table->string('password');
+			$table->string('provider')->nullable();;
+			$table->string('provider_id')->nullable();;
             $table->rememberToken();
             $table->timestamps();
         });
