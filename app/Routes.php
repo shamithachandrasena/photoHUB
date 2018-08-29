2068c22eb5fdc753bf0997ff5f6e7a3c80e59e02
Siyamkhan <42759443+siyamkhan@users.noreply.github.com>
2018-08-29 17:33:48 +0500

    Update Routes.php

--- app/Routes.php
+++ app/Routes.php
@@ -1,1 +1,21 @@
 Route::post('/moveimage', array('as' => 'move_image', 'uses' => 'ImagesController@postMove'));
+
+<?php
+Route::get('/', array('as' => 'index', 'uses' =>
+  'AlbumsController@getList'));
+Route::get('/createalbum', array('as' => 'create_album_form',
+  'uses' => 'AlbumsController@getForm'));
+Route::post('/createalbum', array('as' => 'create_album',
+  'uses' => 'AlbumsController@postCreate'));
+Route::get('/deletealbum/{id}', array('as' => 'delete_album',
+  'uses' => 'AlbumsController@getDelete'));
+Route::get('/album/{id}', array('as' => 'show_album', 'uses' =>
+  'AlbumsController@getAlbum'));
+Route::get('/addimage/{id}', array('as' => 'add_image', 'uses' =>
+  'ImagesController@getForm'));
+Route::post('/addimage', array('as' => 'add_image_to_album',
+  'uses' => 'ImagesController@postAdd'));
+Route::get('/deleteimage/{id}', array('as' => 'delete_image',
+'uses' => 'ImagesController@getDelete'));
+Route::post('/moveimage', array('as' => 'move_image',
+'uses' => 'ImagesController@postMove'));
