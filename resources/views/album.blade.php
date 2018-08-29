13706ff8887b5e9068d302a1db11cf7dbde75beb
shamithachandrasena <33107542+ShamithaChandrasena@users.noreply.github.com>
2018-08-29 18:49:20 +0530

    Changed album view

--- resources/views/album.blade.php
+++ resources/views/album.blade.php
@@ -26,10 +26,10 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="/">Awesome Albums</a>
+        <a class="navbar-brand" href="/">Albums</a>
         <div class="nav-collapse collapse">
           <ul class="nav navbar-nav">
-            <li><a href="{{URL::route('create_album_form')}}">Create New Album</a></li>
+            <li><a href="{{URL::route('create_album_form')}}">Create New</a></li>
           </ul>
         </div><!--/.nav-collapse -->
      </div>
@@ -43,9 +43,9 @@
             <h2 class="media-heading" style="font-size: 26px;">Album Name:</h2>
             <p>{{$album->name}}</p>
           <div class="media">
-          <h2 class="media-heading" style="font-size: 26px;">AlbumDescription :</h2>
+          <h2 class="media-heading" style="font-size: 26px;">Album Description :</h2>
           <p>{{$album->description}}<p>
-          <a href="{{URL::route('add_image',array('id'=>$album->id))}}"><button type="button"class="btn btn-primary btn-large">Add New Image to Album</button></a>
+          <a href="{{URL::route('add_image',array('id'=>$album->id))}}"><button type="button"class="btn btn-primary btn-large">Add Image</button></a>
           <a href="{{URL::route('delete_album',array('id'=>$album->id))}}" onclick="return confirm('Are yousure?')"><button type="button"class="btn btn-danger btn-large">Delete Album</button></a>
         </div>
       </div>
