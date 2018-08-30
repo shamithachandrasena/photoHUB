8272ed5e5b445fe9bb6987b1f951a7948b2e2e7d
shamithachandrasena <33107542+ShamithaChandrasena@users.noreply.github.com>
2018-08-30 16:59:02 +0530

    Modified imagescontroller

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -3,63 +3,63 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-//use App\Album;
-//use App\Image;
+use App\Album;
+use App\Image;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Redirect;
 
 
-class ImagesController extends BaseController{
-
-  public function getForm($id)
-  {
-    $album = Album::find($id);
-    return View::make('addimage')
-    ->with('album',$album);
-  }
-
-  public function postAdd()
-  {
-    $rules = array(
-
-      'album_id' => 'required|numeric|exists:albums,id',
-      'image'=>'required|image'
-
-    );
-    
-    $validator = Validator::make(Input::all(), $rules);
-    if($validator->fails()){
-
-      return Redirect::route('add_image',array('id' =>Input::get('album_id')))
-      ->withErrors($validator)
-      ->withInput();
+class ImagesController extends Controller
+{
+    public function getForm($id)
+    {
+      $album = Album::find($id);
+      return View::make('addimage')
+      ->with('album',$album);
+    }
+  
+    public function postAdd()
+    {
+      $rules = array(
+  
+        'album_id' => 'required|numeric|exists:albums,id',
+        'image'=>'required|image'
+  
+      );
+      
+      $validator = Validator::make(Input::all(), $rules);
+      if($validator->fails()){
+  
+        return Redirect::route('add_image',array('id' =>Input::get('album_id')))
+        ->withErrors($validator)
+        ->withInput();
+      }
+  
+      $file = Input::file('image');
+      $random_name = str_random(8);
+      $destinationPath = 'albums/';
+      $extension = $file->getClientOriginalExtension();
+      $filename=$random_name.'_album_image.'.$extension;
+      $uploadSuccess = Input::file('image')->move($destinationPath, $filename);
+      Image::create(array(
+        'description' => Input::get('description'),
+        'image' => $filename,
+        'album_id'=> Input::get('album_id')
+      ));
+  
+      return Redirect::route('show_album',array('id'=>Input::get('album_id')));
     }
 
-    $file = Input::file('image');
-    $random_name = str_random(8);
-    $destinationPath = 'albums/';
-    $extension = $file->getClientOriginalExtension();
-    $filename=$random_name.'_album_image.'.$extension;
-    $uploadSuccess = Input::file('image')->move($destinationPath, $filename);
-    Image::create(array(
-      'description' => Input::get('description'),
-      'image' => $filename,
-      'album_id'=> Input::get('album_id')
-    ));
+    public function getDelete($id)
+    {
+      $image = Image::find($id);
+      $image->delete();
+      return Redirect::route('show_album',array('id'=>$image->album_id));
+    }
 
-    return Redirect::route('show_album',array('id'=>Input::get('album_id')));
-  }
-  public function getDelete($id)
-  {
-    $image = Image::find($id);
-
-    $image->delete();
-    
-    return Redirect::route('show_album',array('id'=>$image->album_id));
-  }
-  public function postMove()
+    public function postMove()
   {
     $rules = array(
       'new_album' => 'required|numeric|exists:albums,id',
@@ -75,4 +75,5 @@ class ImagesController extends BaseController{
     $image->save();
     return Redirect::route('show_album',array('id'=>Input::get('new_album')));
   }
+
 }
